efe7019e0877a4b921f043e04467e18c87123dea
Eric Crosson <esc@ericcrosson.com>
2015-07-17 02:51:02 -0500

    project legitimized
    exhaustive, sequential search replaces random search

--- rubisim.rb
+++ rubisim.rb
@@ -1,84 +1,35 @@
 #!/usr/bin/env ruby
 require 'rubiks_cube'
-require 'yaml'
 
-VALID_MOVES    = %w(U D L R F B M U' D' L' R' F' B' M').freeze
-INVERSE        = %{'}.freeze
-MOVE_SEPARATOR = %{ }.freeze
-
-def inverse_of(move)
-  if move.nil?
-    nil
-  elsif move.length > 1
-    move[0]
-  else
-    move += INVERSE
+def sequences_of_length(length)
+  %w(U D L R F B U' D' L' R' F' B').combination(length).to_a.reject do |sequence|
+    sequence.each_cons(2).any? { |a,b| a == b.sub("'",'') or b == a.sub("'",'') }
   end
 end
 
-# define methods to generate random legal move sequence of variable
-# length
-def move_sequence(length)
-  # Prevent sequences of zero length
-  length = 1 if length.zero?
-  last_move = nil
-  str = String.new
-  length.times do
-    # Don't move back and forth forever
-    new_move = VALID_MOVES.reject{|m| m == inverse_of(last_move)}.sample
-    str += new_move + MOVE_SEPARATOR
-    last_move = new_move
-  end
-  str
-end
-
-def repeat_sequence_until_solved(scramble)
+def repeat_sequence_until_solved(sequence)
   cube = RubiksCube::Cube.new
-  cube.perform! scramble
+  cube.perform! sequence
   repetitions = 1
   while !cube.solved?
-    cube.perform! scramble
+    cube.perform! sequence
     repetitions += 1
   end
-  repetitions # return number of times to perform scramble until cube
-              # is in solved state
+  repetitions # return number of times to perform sequence until cube
+  # is in solved state
 end
 
-YAML_LOG            = './attempted_trials.yml'
-attempted_trials    = YAML::load(File.read(YAML_LOG)) || Hash.new
-NUMBER_OF_TRIALS    = (ARGV[0] || '10_000_000').to_i.freeze
-max_sequence_length = (ARGV[1] || '1').to_i
-retry_upper_bound   = VALID_MOVES.length ** max_sequence_length
-debug               = ARGV[2].freeze
+length = 0
 
-puts "Starting off adding to the #{attempted_trials.size} existing recorded scrambles"
-
-successful_scrambles = 0
 begin
-  NUMBER_OF_TRIALS.times do
-    scramble = move_sequence(max_sequence_length)
-    need_to_increase_max_sequence_length = 0
-    while attempted_trials.has_key? scramble
-      if (need_to_increase_max_sequence_length += 1) >= retry_upper_bound
-        max_sequence_length += 1
-        retry_upper_bound = VALID_MOVES.length ** max_sequence_length
-        puts "Increasing max_sequence_length to #{max_sequence_length}"
-      end
-      scramble = move_sequence(max_sequence_length)
+  loop do
+    length += 1
+    puts "Starting trials of length #{length}"
+    sequences = sequences_of_length(length)
+    sequences.each do |seq|
+      reps = repeat_sequence_until_solved(seq.join(' '))
+      puts "Sequence #{seq} took #{reps} repetitions to reset"
     end
-    puts "Detected new scramble: #{scramble}"
-    repeats_until_solved = repeat_sequence_until_solved(scramble)
-    puts "  iterations until solved: #{repeats_until_solved}"
-    puts
-
-    # Record trial
-    attempted_trials[scramble] = repeats_until_solved
-    successful_scrambles += 1
   end
 rescue Interrupt, SystemExit
-  puts "You opted to terminate program execution after #{successful_scrambles} successful scrambles"
-ensure
-  # Save new trial runs
-  File.open(YAML_LOG, 'w') {|f| f.write attempted_trials.to_yaml}
-  puts "Total recorded successful scrambles: #{attempted_trials.size}"
 end
